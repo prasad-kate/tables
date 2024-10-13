af8e41dcbd11536d7717ab0bfde5f087e9124861
prasadk <prasadkatepk@gmail.com>
2024-10-13 20:58:58 +0530

    feat: routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  return <>Hello world</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* for unhandled route */}
+        <Route path="*" element={<>Page not found</>} />
+
+        {/* routes */}
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
