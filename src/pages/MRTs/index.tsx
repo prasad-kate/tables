7cd206e6c08b2eb3f90323b3604a1a4e8d9acb3e
prasadk <prasadkatepk@gmail.com>
2024-10-18 23:22:57 +0530

    feat: add inline row edit type mrt table

--- src/pages/MRTs/index.tsx
+++ src/pages/MRTs/index.tsx
@@ -2,6 +2,7 @@ import withRootLayout from "../../HOCs/withRootLayout";
 import BasicTable from "./components/BasicTableMrt";
 import ColumnOrderingTable from "./components/ColumnOrderingMrt";
 import ExportToCsvTable from "./components/ExcelExportTypeMrt";
+import InlineRowEditTypeTable from "./components/InlineRowEditMrt";
 import ModalExportTypeTable from "./components/ModalEditingTypeMrt";
 import ExportToPdfTable from "./components/PdfExportTypeMrt";
 import RowDraggingTable from "./components/RowDraggingMrt";
@@ -17,6 +18,7 @@ function MaterialReactTable() {
       <RowOrderingTable />
       <RowDraggingTable />
       <ModalExportTypeTable />
+      <InlineRowEditTypeTable />
     </div>
   );
 }
